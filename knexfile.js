6ef20862da327e9d29a68ad9f09dc047764bcdd4
clay selby <claytonrselby@gmail.com>
2020-12-14 13:06:21 -0600

    set prod database

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,22 @@
 module.exports = {
 
+  // development: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host: 'localhost',
+  //     user: 'root',
+  //     database: 'stonks',
+  //   },
+  // },
   development: {
     client: 'mysql',
-    connection: {
-      host: 'localhost',
-      user: 'root',
-      database: 'stonks',
+    connection: 'mysql://b740f8f4f5943d:e6a5cae5@us-cdbr-east-02.cleardb.com/heroku_5021072f724a751?reconnect=true',
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
     },
   },
 
